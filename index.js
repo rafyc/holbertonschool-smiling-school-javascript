600ff74a694f44a1150ea411dd1bb452e6e3f950
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-20 15:29:40 +0200

    +1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+const menuBtn = document.querySelector('.menu-btn');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+menuBtn.addEventListener('click', function () {
+  navbarMenu.classList.toggle('show');
+});
