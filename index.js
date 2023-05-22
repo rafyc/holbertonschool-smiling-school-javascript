aa2b47e14f7bc5eab1e2275366e9c36af6f60997
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-22 09:50:30 +0200

    +slider in progress

--- index.js
+++ index.js
@@ -5,3 +5,47 @@ const navbarMenu = document.querySelector('.navbar-menu');
 menuBtn.addEventListener('click', function () {
   navbarMenu.classList.toggle('show');
 });
+
+// Slider animation
+
+const slider = document.querySelector('.slider');
+const slide = document.querySelectorAll('.slide')
+const slideCount = slide.length;
+
+let newTranslate = 0
+let counter = 0
+
+const next = () => {
+  if (counter == 0) {
+    slider.style["flex-direction"] = 'row'
+  }
+
+  newTranslate -= 100
+  slider.classList.add('slider-transition');
+  slider.style.transform = `translateX(${newTranslate}%)`
+  counter++
+  console.log(counter);
+  console.log(slideCount);
+  if (counter == slideCount) {
+    slider.classList.remove('slider-transition');
+    slider.style.transform = `translateX(0%)`
+    newTranslate = 0
+    counter = 0
+  }
+};
+
+const prev = () => {
+  console.log('prev');
+  slider.style["flex-direction"] = 'row-reverse'
+  newTranslate += 100
+  slider.classList.add('slider-transition');
+  slider.style.transform = `translateX(${newTranslate}%)`
+  counter--
+  console.log(counter);
+  if (counter == slideCount * -1) {
+    slider.classList.remove('slider-transition');
+    slider.style.transform = `translateX(0%)`
+    newTranslate = 0
+    counter = 0
+  }
+};
