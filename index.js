e45a643d56e58460919e57636c115aff9b87a423
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-22 11:19:54 +0200

    +slider

--- index.js
+++ index.js
@@ -8,44 +8,35 @@ menuBtn.addEventListener('click', function () {
 
 // Slider animation
 
-const slider = document.querySelector('.slider');
-const slide = document.querySelectorAll('.slide')
-const slideCount = slide.length;
+// Here we're going to move the active class between the slides. You can do this however you want, but for brevity I'm using JQuery.
 
-let newTranslate = 0
-let counter = 0
+// Get all the slides
+var slides = $('.slide');
 
-const next = () => {
-  if (counter == 0) {
-    slider.style["flex-direction"] = 'row'
+// Move the last slide before the first so the user is able to immediately go backwards
+slides.first().before(slides.last());
+
+$('.slider-nav').on('click', function () {
+  // Get all the slides again
+  slides = $('.slide');
+  // Register button
+  var button = $(this);
+  // Register active slide
+  var activeSlide = $('.active');
+
+  // Next function
+  if (button.attr('id') == 'next') {
+    // Move first slide to the end so the user can keep going forward
+    slides.last().after(slides.first());
+    // Move active class to the right
+    activeSlide.removeClass('active').next('.slide').addClass('active');
   }
 
-  newTranslate -= 100
-  slider.classList.add('slider-transition');
-  slider.style.transform = `translateX(${newTranslate}%)`
-  counter++
-  console.log(counter);
-  console.log(slideCount);
-  if (counter == slideCount) {
-    slider.classList.remove('slider-transition');
-    slider.style.transform = `translateX(0%)`
-    newTranslate = 0
-    counter = 0
+  // Previous function
+  if (button.attr('id') == 'prev') {
+    // Move the last slide before the first so the user can keep going backwards
+    slides.first().before(slides.last());
+    // Move active class to the left
+    activeSlide.removeClass('active').prev('.slide').addClass('active');
   }
-};
-
-const prev = () => {
-  console.log('prev');
-  slider.style["flex-direction"] = 'row-reverse'
-  newTranslate += 100
-  slider.classList.add('slider-transition');
-  slider.style.transform = `translateX(${newTranslate}%)`
-  counter--
-  console.log(counter);
-  if (counter == slideCount * -1) {
-    slider.classList.remove('slider-transition');
-    slider.style.transform = `translateX(0%)`
-    newTranslate = 0
-    counter = 0
-  }
-};
+});
