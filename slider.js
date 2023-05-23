7c0d31ca8f7aa5d6a23e60132516a50da4941254
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-23 22:14:31 +0200

    +slider responsive in prgress

---
+++ slider.js
@@ -0,0 +1,212 @@
+const getCourse = () => {
+  var xhttp = new XMLHttpRequest();
+  var loader = document.getElementById("course-loader");
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonArray = JSON.parse(this.responseText);
+      let length = jsonArray.length
+      console.log(jsonArray[0]);
+      for (let i = 0; i < length; i++) {
+        let index = i
+        let image = jsonArray[i].thumb_url
+        let titre = jsonArray[i].title
+        let desc = jsonArray[i]['sub-title'];
+        let author = jsonArray[i].author
+        let authorPic = jsonArray[i].author_pic_url
+        let rating = jsonArray[i].star
+        let time = jsonArray[i].duration
+        constructCourse(image, titre, desc, author, authorPic, rating, time)
+      }
+      createCourseSlider()
+      loader.style.display = "none";
+    }
+  }
+  url = 'https://smileschool-api.hbtn.info/popular-tutorials'
+  setTimeout(function () {
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }, 2000)
+};
+
+const constructCourse = (image, titre, desc, author, authorPic, rating, time) => {
+  // Create the first <div> element with class "video_element"
+  var divVideoElement1 = document.createElement("div");
+  divVideoElement1.classList.add("video_element");
+  console.log('created');
+
+  // Create the first <div> element with class "player_container"
+  var divPlayerContainer1 = document.createElement("div");
+  divPlayerContainer1.classList.add("player_container");
+
+  // Create the first <div> element with class "video_content"
+  var divVideoContent1 = document.createElement("div");
+  divVideoContent1.classList.add("video_content");
+
+  // Create the <img> element with src and alt attributes
+  var imgPreview1 = document.createElement("img");
+  imgPreview1.src = image;
+  imgPreview1.alt = "";
+
+  // Create the <img> element for the play button
+  var imgPlay1 = document.createElement("img");
+  imgPlay1.src = "./assets/play.png";
+  imgPlay1.alt = "player";
+  imgPlay1.classList.add("player");
+
+  // Create the <div> element with class "video_author"
+  var divVideoAuthor1 = document.createElement("div");
+  divVideoAuthor1.classList.add("video_author");
+
+  // Create the <h2> element for the video title
+  var h2Title1 = document.createElement("h2");
+  h2Title1.innerText = titre;
+
+  // Create the <p> element for the video description
+  var pDescription1 = document.createElement("p");
+  pDescription1.innerText = desc;
+
+  // Create the <div> element with class "author"
+  var divAuthor1 = document.createElement("div");
+  divAuthor1.classList.add("author");
+
+  // Create the <img> element for the teacher avatar
+  var imgAvatar1 = document.createElement("img");
+  imgAvatar1.src = authorPic;
+  imgAvatar1.alt = "teacher";
+  imgAvatar1.classList.add("min_avatar");
+
+  // Create the <h3> element for the teacher's name
+  var h3TeacherName1 = document.createElement("h3");
+  h3TeacherName1.innerText = author;
+
+  // Create the <div> element with class "rating"
+  var divRating1 = document.createElement("div");
+  divRating1.classList.add("rating");
+
+  // Create the <div> element for the star ratings
+  var divStars1 = document.createElement("div");
+  // Create and append the star images
+  let fullStar = 5
+  let starRating = rating
+  for (var i = 0; i < rating; i++) {
+    var imgStar1 = document.createElement("img");
+    imgStar1.src = "./assets/Star.png";
+    imgStar1.alt = "star";
+    divStars1.appendChild(imgStar1);
+  }
+  // Create the gray star image
+  for (let i = 0; i < fullStar - starRating; i++) {
+    var imgStarGrey1 = document.createElement("img");
+    imgStarGrey1.src = "./assets/Star_grey.png";
+    imgStarGrey1.alt = "star";
+    divStars1.appendChild(imgStarGrey1);
+  }
+
+
+  // Create the <p> element for the video duration
+  var pDuration1 = document.createElement("p");
+  pDuration1.innerText = time;
+
+  // Append the elements to build the structure
+  divAuthor1.appendChild(imgAvatar1);
+  divAuthor1.appendChild(h3TeacherName1);
+
+  divRating1.appendChild(divStars1);
+  divRating1.appendChild(pDuration1);
+
+  divVideoAuthor1.appendChild(h2Title1);
+  divVideoAuthor1.appendChild(pDescription1);
+  divVideoAuthor1.appendChild(divAuthor1);
+  divVideoAuthor1.appendChild(divRating1);
+
+  divVideoContent1.appendChild(imgPreview1);
+  divVideoContent1.appendChild(imgPlay1);
+  divVideoContent1.appendChild(divVideoAuthor1);
+
+  divPlayerContainer1.appendChild(divVideoContent1)
+  divVideoElement1.appendChild(divPlayerContainer1)
+
+  var videosContainer = document.querySelector('.video_container');
+  videosContainer.appendChild(divVideoElement1);
+}
+
+
+const createCourseSlider = () => {
+
+  window.addEventListener("resize", function () {
+    result = displayWindowSize();
+    console.log(result);
+    let container = document.getElementById('hidden')
+    let hiddenDiv = document.createElement('span')
+    hiddenDiv.textContent = result
+    container.appendChild(hiddenDiv)
+    if (container.childNodes.length > 1) {
+      container.removeChild(container.childNodes[0]);
+    }
+  });
+
+  // Main elements
+  let container = document.querySelector('.video')
+  let videoElement = document.querySelector('.video_element');
+  let carousel = document.querySelector('.video_container');
+  let left = document.querySelector('.video-arrow-left');
+  let right = document.querySelector('.video-arrow-right');
+  let item = document.querySelector('.video_element');
+  // how many elements to scroll
+  let n = 1;
+  let num = 0
+
+  // Helper functions: show/hide an element
+  // var show = function (elem) {
+  //   elem.style.visibility = 'visible';
+  // };
+  // var hide = function (elem) {
+  //   elem.style.visibility = 'hidden';
+  // };
+
+  // Carousel arrows left/right
+  right.addEventListener('click', function () {
+    num -= n * item.clientWidth
+    carousel.style.transform = `translateX(${num}px)`
+  });
+  left.addEventListener('click', function () {
+    num += n * item.clientWidth
+    carousel.style.transform = `translateX(${num}px)`
+  });
+
+  // responsive
+  let videoElementWidth = videoElement.clientWidth
+
+  let contain = document.getElementById('hidden')
+  let span = contain.getElementsByTagName('span')[0]
+  let rez = span.textContent
+  console.log(`val${rez}`);
+
+  let nbItemDisplayed = 4
+
+  let sliderWidth = nbItemDisplayed * videoElementWidth
+  container.style.maxWidth = `${sliderWidth}px`
+
+  // window size helper
+  function displayWindowSize() {
+    // Get width and height of the window excluding scrollbars
+    var w = document.documentElement.clientWidth;
+
+    if (w <= 1330) {
+      nbItemDisplayed = 3;
+    } else if (w <= 900) {
+      nbItemDisplayed = 2;
+    } else {
+      nbItemDisplayed = 4;
+    }
+    return nbItemDisplayed
+  }
+
+
+
+
+
+
+}
+getCourse()
+initResponsive()
